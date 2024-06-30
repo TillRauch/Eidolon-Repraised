56f9b9773a327f6f6c9334420ce9389551230484
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-06-30 12:08:08 +0200

    update apotheosis compat

--- src/main/java/elucent/eidolon/compat/apotheosis/HailingAffix.java
+++ src/main/java/elucent/eidolon/compat/apotheosis/HailingAffix.java
@@ -4,19 +4,19 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
 import dev.shadowsoffire.apotheosis.adventure.affix.AffixType;
-import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
+import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
 import dev.shadowsoffire.placebo.util.StepFunction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.function.Consumer;
 
 public class HailingAffix extends Affix implements Apotheosis.StepScalingAffix {
-    public static final Codec<HailingAffix> CODEC = RecordCodecBuilder.create(inst -> inst.group(GemBonus.VALUES_CODEC.fieldOf("values").forGetter((a) -> a.values)).apply(inst, HailingAffix::new));
+    public static final Codec<HailingAffix> CODEC = RecordCodecBuilder.create(inst -> inst.group(GemBonus.VALUES_CODEC.fieldOf("values").forGetter(a -> a.values)).apply(inst, HailingAffix::new));
     protected final Map<LootRarity, StepFunction> values;
 
     public HailingAffix(Map<LootRarity, StepFunction> values) {
@@ -30,8 +30,8 @@ public class HailingAffix extends Affix implements Apotheosis.StepScalingAffix {
     }
 
     @Override
-    public void addInformation(final ItemStack stack, final LootRarity rarity, float level, final Consumer<Component> list) {
-        list.accept(Component.translatable("affix." + this.getId() + ".desc", fmt(affixToAmount(rarity, level))));
+    public MutableComponent getDescription(final ItemStack stack, final LootRarity rarity, float level) {
+        return Component.translatable("affix." + this.getId() + ".desc", fmt(affixToAmount(rarity, level)));
     }
     @Override
     public Codec<? extends Affix> getCodec() {
--- src/main/java/elucent/eidolon/compat/apotheosis/TrackingAffix.java
+++ src/main/java/elucent/eidolon/compat/apotheosis/TrackingAffix.java
@@ -4,16 +4,16 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
 import dev.shadowsoffire.apotheosis.adventure.affix.AffixType;
-import dev.shadowsoffire.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
+import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
 import dev.shadowsoffire.placebo.util.StepFunction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.function.Consumer;
 
 public class TrackingAffix extends Affix implements Apotheosis.StepScalingAffix {
 
@@ -37,8 +37,8 @@ public class TrackingAffix extends Affix implements Apotheosis.StepScalingAffix 
     }
 
     @Override
-    public void addInformation(final ItemStack stack, final LootRarity rarity, float level, final Consumer<Component> list) {
-        list.accept(Component.translatable("affix." + this.getId() + ".desc", fmt(affixToAmount(rarity, level))));
+    public MutableComponent getDescription(final ItemStack stack, final LootRarity rarity, float level) {
+        return Component.translatable("affix." + this.getId() + ".desc", fmt(affixToAmount(rarity, level)));
     }
 
     @Override
